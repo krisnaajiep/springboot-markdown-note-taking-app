59a7a9d22f2b03d3aa60db30527d7c57592295c1
Krisna Ajie <krisnaajiep@gmail.com>
2026-05-29 21:58:00 +0700

    first commit

---
+++ src/main/java/com/krisnaajiep/markdownnotetaking/MarkdownNoteTakingApplication.java
@@ -0,0 +1,13 @@
+package com.krisnaajiep.markdownnotetaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkdownNoteTakingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarkdownNoteTakingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/krisnaajiep/markdownnotetaking/MarkdownNoteTakingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krisnaajiep.markdownnotetaking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkdownNoteTakingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
