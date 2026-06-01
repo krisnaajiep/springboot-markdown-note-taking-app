17db24414d21fe3a009ec6942319cd6e75432121
Krisna Ajie <krisnaajiep@gmail.com>
2026-06-02 00:22:40 +0700

    feat(note): implement render method in `MarkdownNoteService` to return HTML string and add Markdown parsing capabilities
    - Update project version to `0.0.3-SNAPSHOT` in `pom.xml` file.
    - Add `flexmark-java` dependency for HTML rendering.

--- src/main/java/com/krisnaajiep/markdownnotetaking/service/MarkdownNoteService.java
+++ src/main/java/com/krisnaajiep/markdownnotetaking/service/MarkdownNoteService.java
@@ -8,6 +8,14 @@ import com.krisnaajiep.markdownnotetaking.model.NoteRepository;
 import com.krisnaajiep.markdownnotetaking.service.grammar.GrammarCheckService;
 import com.krisnaajiep.markdownnotetaking.service.storage.StorageService;
 import com.krisnaajiep.markdownnotetaking.validator.MarkdownFileValidator;
+import com.vladsch.flexmark.ext.autolink.AutolinkExtension;
+import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
+import com.vladsch.flexmark.ext.gfm.tasklist.TaskListExtension;
+import com.vladsch.flexmark.ext.tables.TablesExtension;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.ast.Document;
+import com.vladsch.flexmark.util.data.MutableDataSet;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +24,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -68,7 +77,25 @@ public class MarkdownNoteService implements NoteService {
     }
 
     @Override
-    public void render(String filename) {
+    public String render(String filename) throws IOException {
+        MutableDataSet options = new MutableDataSet();
 
+        options.set(
+                Parser.EXTENSIONS,
+                Arrays.asList(
+                        TablesExtension.create(),
+                        StrikethroughExtension.create(),
+                        TaskListExtension.create(),
+                        AutolinkExtension.create()
+                )
+        );
+
+        String content = storageService.load(filename);
+
+        Parser parser = Parser.builder(options).build();
+        HtmlRenderer renderer = HtmlRenderer.builder(options).build();
+
+        Document parse = parser.parse(content);
+        return renderer.render(parse);
     }
 }
